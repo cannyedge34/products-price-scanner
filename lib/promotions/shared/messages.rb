cd580804c8d3080ad056360a9c639e04459f4b99
cannyedge34 <cannyedge34@gmail.com>
2023-08-22 12:23:11 +0200

    Add promotion shared messages

---
+++ lib/promotions/shared/messages.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Promotions
+  module Shared
+    module Messages
+      BULK_SLUG = 'bulk-purchase'
+      BUY_ONE_GET_ONE_SLUG = 'buy-one-get-one-free'
+    end
+  end
+end
